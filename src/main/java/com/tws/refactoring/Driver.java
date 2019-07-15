2faa22dd062c030de6079331946bfb5fd068b7bd
FelicityChen <felicity.chen@oocl.com>
2019-07-15 15:37:29 +0800

    Refactor: move the check driver logic to driver class

--- src/main/java/com/tws/refactoring/Driver.java
+++ src/main/java/com/tws/refactoring/Driver.java
@@ -1,9 +1,17 @@
 package com.tws.refactoring;
 
 public class Driver {
-    public int age;
+    private int age;
 
     public Driver(int age) {
         this.age = age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isLegalDriver() {
+        return this.age >= 18;
     }
 }
--- src/test/java/com.tws.refactoring/DriverTest.java
+++ src/test/java/com.tws.refactoring/DriverTest.java
@@ -3,35 +3,31 @@ package com.tws.refactoring;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class PoliceTest {
-
+class DriverTest {
     private Driver driver;
-    private Police police;
 
     @Before
     public void setUp(int age) {
         this.driver = new Driver(age);
-        this.police = new Police();
     }
 
     @Test
     public void should_return_true_when_call_checkDriver_given_19() {
         setUp(19);
-        assertEquals(true, police.checkDriver(driver));
+        assertEquals(true, driver.isLegalDriver());
     }
 
     @Test
     public void should_return_true_when_call_checkDriver_given_18() {
         setUp(18);
-        assertEquals(true, police.checkDriver(driver));
+        assertEquals(true, driver.isLegalDriver());
     }
 
     @Test
     public void should_return_false_when_call_checkDriver_given_17() {
         setUp(17);
-        boolean result = police.checkDriver(driver);
-        assertEquals(false, result);
+        assertEquals(false, driver.isLegalDriver());
     }
 }
